b4c32e5a14f6f457a56f6387ffa0b84531bb4095
dotansimha <dotansimha@gmail.com>
2017-06-21 14:12:06 +0300

    fixes

--- src/strest.ts
+++ src/strest.ts
@@ -4,4 +4,8 @@ import * as jest from 'jest-cli';
 
 const args = [...process.argv.splice(2), '--forceExit', '-i', '--verbose', '--runInBand'];
 
+if (process.env.DEBUG) {
+  console.log('Executing Jest with arguments: ', args);
+}
+
 jest.run(args);
